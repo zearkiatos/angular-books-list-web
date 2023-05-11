63d1cb3c6ee71b61d2fc46ec84967b8d46cdfc13
zearkiatos <caprilespe@outlook.com>
2023-05-10 21:51:55 -0400

    Define form object in the create component

--- src/app/author/author-create/author-create.component.ts
+++ src/app/author/author-create/author-create.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-author-create',
   templateUrl: './author-create.component.html',
-  styleUrls: ['./author-create.component.css']
+  styleUrls: ['./author-create.component.css'],
 })
 export class AuthorCreateComponent implements OnInit {
-
-  constructor() { }
+  authorForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
+    this.authorForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      image: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      description: ['', [Validators.required, Validators.maxLength(100)]],
+    });
   }
-
 }

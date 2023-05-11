ed0e3331f2ae729ef3eb3f85cac2cb5613124ca0
zearkiatos <caprilespe@outlook.com>
2023-05-10 20:51:26 -0400

    Fix tests

--- src/interceptors/HttpErrorInterceptor.service.spec.ts
+++ src/interceptors/HttpErrorInterceptor.service.spec.ts
@@ -1,16 +1,21 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { ToastrModule } from 'ngx-toastr';
 import { HttpErrorInterceptorService } from './HttpErrorInterceptor.service';
 
 describe('Service: HttpErrorInterceptor', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpErrorInterceptorService]
+      providers: [HttpErrorInterceptorService],
+      imports: [ToastrModule.forRoot()],
     });
   });
 
-  it('should ...', inject([HttpErrorInterceptorService], (service: HttpErrorInterceptorService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should ...', inject(
+    [HttpErrorInterceptorService],
+    (service: HttpErrorInterceptorService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
 });

a8833eacf81133e33121f076cb1c1b0fa38b8a41
zearkiatos <caprilespe@outlook.com>
2023-05-10 21:45:45 -0400

    Call create component

--- src/app/author/author.routing.ts
+++ src/app/author/author.routing.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthorDetailComponent } from './author-detail/author-detail.component';
+import { AuthorCreateComponent } from './author-create/author-create.component';
 
 const routes: Routes = [
   {
     path: 'authors',
     children: [
+      {
+        path: 'create',
+        component: AuthorCreateComponent,
+      },
       {
         path: ':id',
         component: AuthorDetailComponent,
